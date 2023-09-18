84f461a10d2928cece481afbb538382b013b5371
alariaso <141962262+alariaso@users.noreply.github.com>
2023-09-18 21:51:02 +0000

    agregar clase zoologico

---
+++ gestion/zoologico.py
@@ -0,0 +1,29 @@
+class Zoologico:
+    def __init__(self, nombre, ubicacion) -> None:
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
+    def cantidadTotalAnimales(self):
+        total = 0
+        for zona in self._zonas:
+            total += zona.cantidadAnimales()
+        return total
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion
+
+    def getZona(self):
+        return self._zonas
