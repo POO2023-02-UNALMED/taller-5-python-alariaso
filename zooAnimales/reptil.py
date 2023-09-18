9d9404c860992d7904c0633b83288ee2cc9e9445
alariaso <141962262+alariaso@users.noreply.github.com>
2023-09-18 22:39:30 +0000

    agregar clase reptil

---
+++ zooAnimales/reptil.py
@@ -0,0 +1,38 @@
+from .animal import Animal
+
+class Reptil(Animal):
+    _listado = []
+    iguanas = 0
+    serpientes = 0
+
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola) -> None:
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._largoCola = largoCola
+        Reptil._listado.append(self)
+
+    @classmethod
+    def cantidadReptiles(cls):
+        return len(cls._listado)
+
+    @staticmethod
+    def crearIguana(nombre, edad, genero):
+        Reptil.iguanas += 1
+        return Reptil(nombre, edad, "humedal", genero, "verde", 3)
+
+    @staticmethod
+    def crearSerpiente(nombre, edad, genero):
+        Reptil.serpientes += 1
+        return Reptil(nombre, edad, "jungla", genero, "blanco", 1)
+
+    def getColorEscamas(self):
+        return self._colorEscamas
+
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
+
+    def getLargoCola(self):
+        return self._largoCola
+
+    def setLargoCola(self, largoCola):
+        self._largoCola = largoCola
