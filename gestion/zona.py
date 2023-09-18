0b81ca79917003dd14b5485c05afe1069fb6629b
alariaso <141962262+alariaso@users.noreply.github.com>
2023-09-18 22:52:18 +0000

    arreglar constructor de Zona

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,5 +1,5 @@
 class Zona:
-    def __init__(self, nombre, zoo) -> None:
+    def __init__(self, nombre, zoo=None) -> None:
         self._nombre = nombre
         self._zoo = zoo
         self._animales = []
